d375a352f8296c63e2b730e7cc3513f7fedf1dc4
Elma-Eimy <2075274894@qq.com>
2026-05-27 00:46:51 +0800

    Add files via upload

---
+++ app-frontend/src/main.ts
@@ -0,0 +1,16 @@
+import { createSSRApp } from "vue";
+import { createPinia } from "pinia";
+import "virtual:uno.css";
+import App from "./App.vue";
+
+export function createApp() {
+  const app = createSSRApp(App);
+
+  // 安装 Pinia 全局状态管理库
+  const pinia = createPinia();
+  app.use(pinia);
+
+  return {
+    app,
+  };
+}
